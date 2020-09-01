5744adf9085021e829abb76a6098c821f79f134d
Durgarao Kota <dkota@appstekcorp.com>
2020-09-01 13:38:54 -0400

    just testing purpose

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> durga
+          Edit <code>src/App.js</code> durgarao
         </p>
         <a
           className="App-link"
